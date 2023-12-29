985dc035254cd58c98f1c83d7f846b42cca9c3cf
Vibs <vibs2006@gmail.com>
2023-12-29 22:05:39 +0530

    Product Repository Part 1

---
+++ Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,76 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository, IBrandRepository, ITypesRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext catalogContext)
+        {
+            _context = catalogContext;
+        }
+        public async Task<Product> CreateProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+            return product;
+        }
+
+        public Task<bool> DeleteProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductBrand>> GetAllBrands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductType>> GetAllTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByBrand(string name)
+        {
+            FilterDefinition<Product> filter = 
+                Builders<Product>.Filter.Eq(p => p.Brands != null ? p.Brands.Name : "", name);
+
+            return await _context
+                .Products
+                .Find(filter)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            //return await _context.Products.Find(p => p.Name == name).ToListAsync();
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.Find(x => true).ToListAsync();                
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updateResult = await _context.Products.ReplaceOneAsync(p => p.Id == product.Id, product);
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+    }
+}
